33e9438cca3cd6a3433c39408a4b0f0fb441dd46
Theodore Chang <tlcfem@gmail.com>
2023-11-24 01:44:27 +0100

    [publish] Update metadata

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 from datetime import datetime
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 # noinspection PyTypeChecker
 setup(
@@ -10,6 +14,8 @@ setup(
     author_email="tlcfem@gmail.com",
     url="https://github.com/TLCFEM/vpmr",
     description="The VPMR Algorithm",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     ext_modules=[
         Pybind11Extension(
             "pyvpmr",
