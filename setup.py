39c452457b58e7e47f397981c5d77074efd78a2b
Theodore Chang <tlcfem@gmail.com>
2024-09-21 17:21:32 +0200

    Ignore files, ensure patch

--- setup.py
+++ setup.py
@@ -8,17 +8,28 @@ from setuptools import setup
 # noinspection PyTypeChecker
 setup(
     version=datetime.now().strftime("%y%m%d"),
-    long_description=(Path(__file__).parent / "README.md").read_text(encoding='utf8').replace(
-        "resource/", "https://raw.githubusercontent.com/TLCFEM/vpmr/master/resource/"),
-    long_description_content_type='text/markdown',
+    long_description=(Path(__file__).parent / "README.md")
+    .read_text(encoding="utf8")
+    .replace(
+        "resource/", "https://raw.githubusercontent.com/TLCFEM/vpmr/master/resource/"
+    ),
+    long_description_content_type="text/markdown",
     ext_modules=[
         Pybind11Extension(
             "_pyvpmr",
             ["src/VPMR.cpp"],
-            include_dirs=["eigen", "eigen/unsupported", "mpreal", "exprtk", "exprtk-custom-types"],
+            include_dirs=[
+                "eigen",
+                "eigen/unsupported",
+                "mpreal",
+                "exprtk",
+                "exprtk-custom-types",
+            ],
             libraries=["mpfr", "gmp", "tbb"],
             define_macros=[("PYVPMR", 1)],
-            extra_compile_args=['-fno-aligned-allocation'] if "Darwin" == platform.system() else [],
+            extra_compile_args=["-fno-aligned-allocation"]
+            if "Darwin" == platform.system()
+            else [],
         ),
     ],
     cmdclass={"build_ext": build_ext},
